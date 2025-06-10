b7e97cacccd8e87011203bf5f8723e1d23889d41
unknown <e23cseu0624@bennett.edu.in>
2025-06-10 15:34:46 +0530

    express

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('Mongo error:', err));
