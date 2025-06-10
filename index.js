f48ba62f7291adeb5037695b34c8ed895ba15d78
unknown <e23cseu0624@bennett.edu.in>
2025-06-10 17:56:12 +0530

    Added token validation

--- index.js
+++ index.js
@@ -22,7 +22,11 @@ mongoose.connect(process.env.MONGO_URI)
 app.post('/generate-token', async (req, res) => {
   const phonenumber = req.body.phonenumber;
   const token = uuidv4();
-  const newLogin = new Login({ token, phonenumber });
+  const currtime = new Date().toISOString();
+  const expirytime = new Date(Date.now() + 5 * 60 * 1000).toISOString();
+
+
+  const newLogin = new Login({ token, phonenumber ,currtime, expirytime });
   await newLogin.save();
   const whatsappMessage = `Hi, my login token is: ${token}`;
   const encodedMessage = encodeURIComponent(whatsappMessage);
@@ -32,12 +36,52 @@ app.post('/generate-token', async (req, res) => {
   res.json({
     phonenumber,
     token,
+    currtime,
+    expirytime,
     link: whatsappLink
   });
 });
 
 
+app.get('/verify-token', async (req, res) => {
+  const { token } = req.query;
 
+  if (!token) {
+    return res.status(400).json({ error: 'Token is required' });
+  }
+
+  try {
+    const login = await Login.findOne({ token });
+
+    if (!login) {
+      return res.status(404).json({ error: 'Token not found' });
+    }
+
+    if (login.loginstatus) {
+      return res.redirect('used.html');
+    }
+
+    const loginTime = new Date();
+    const expiryTime = new Date(login.expirytime);
+
+    if (new Date(loginTime) > new Date(expiryTime)) {
+      return res.status(400).json({ error: 'Token has expired' });
+    }
+
+    login.loginstatus = true;
+    await login.save();
+
+    // res.json({
+    //   phonenumber: login.phonenumber,
+    //   logintime: loginTime,
+    //   expirytime: login.expirytime
+    // });
+    res.redirect('login.html');
+  } catch (error) {
+    console.error('Error verifying token:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
--- models/Login.js
+++ models/Login.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const loginSchema = new mongoose.Schema({
   token: { type: String, required: true, unique: true },
   phonenumber: { type: String, required: true },
-  used: { type: Boolean, default: false },
+  currtime: { type: Date, default: Date.now },
+  expirytime: { type: Date, default: Date.now, expires: '1h' }, // Expires after 1 hour
+  loginstatus: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('Login', loginSchema);
